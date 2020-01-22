c5e44c575f19b5131beeb96f853ec76c913c6448
Sharnell Grant <sharnellgrant@gmail.com>
2020-01-22 04:41:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ end
 
 
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_types)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.include? { |item| 
 end
