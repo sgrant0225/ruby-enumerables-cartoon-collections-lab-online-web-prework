10f0ac48bc2c20d22493e764c9751fc3f898f695
Sharnell Grant <sharnellgrant@gmail.com>
2020-01-22 04:23:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
   end
 
 def long_planeteer_calls(array_of_calls)
-  array_of_calls.any? do |item| item.size == 4 
+  if array_of_calls.any? do |item| item.size == 4 
 end
 end
 
