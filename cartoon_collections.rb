bea263118fad87ae48e2b667ea36c1e924cc41d0
Sharnell Grant <sharnellgrant@gmail.com>
2020-01-22 04:21:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(array)
 
 def long_planeteer_calls(array_of_calls)
   array_of_calls.any? do |item| item.size >= 4 
-
+end
 end
 
 def find_the_cheese# code an argument here
