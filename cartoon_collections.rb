500bd6213c10c48f7d3d44835b82c0229ef3ed39
Sharnell Grant <sharnellgrant@gmail.com>
2020-01-22 04:52:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,6 @@ end
 def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  strings.find do |cheese|
+  strings.find do |cheese| 
+    
 end
